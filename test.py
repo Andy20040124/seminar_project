87b10b5d6b348d69f22f30c64c8b2e20f5e9a998
HOU-YU-ZHANG <andyvip0124@example.com>
2025-03-27 23:55:04 +0800

    demo pass

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import requests
 import shutil
 import nltk
+""" !pip install spacy
+!python -m spacy download en_core_web_sm
+!pip install -U NLTK
+nltk.download('punkt_tab')
+!pip install scipy
+!pip install sumy """
+import sumy
 from bs4 import BeautifulSoup # get clean data
 from googlesearch import search #google search for the content
 from sumy.parsers.plaintext import PlaintextParser
@@ -8,7 +15,7 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.edmundson import EdmundsonSummarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
-nltk.data.path.append("C:/Users/andy/AppData/Roaming/nltk_data")
+nltk.download('punkt_tab')
 
 url = 'https://pipingtech.com/resources/faqs/what-is-u-stamp-certification/'
 keyword = "U-stamp"
@@ -20,23 +27,24 @@ paragraph = [ct.get_text(strip=True) for ct in dived if keyword in ct.get_text()
 
 text = " ".join(paragraph)
 
-# 設定語言
+# 設計語言以及懲罰詞(有缺點)
 LANGUAGE = "english"
-bonus_words = ["important", "significant", "key", keyword]  # 重要詞
-stigma_words = ["example", "however", "thus"]  # 懲罰詞
+bonus_words = ["important", "significant", keyword]  # 重要詞
+stigma_words = ["example", "however", "thus","SupportsExpansion","Gallery","TX",".com"]  # 懲罰詞
 null_words = get_stop_words(LANGUAGE)  # 停用詞
 
-# 創建解析器
+
 parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
 
-# Edmundson 摘要器
+# https://www.geeksforgeeks.org/mastering-text-summarization-with-sumy-a-python-library-overview/
 summarizer = EdmundsonSummarizer(Stemmer(LANGUAGE))
 summarizer.bonus_words = bonus_words
 summarizer.stigma_words = stigma_words
 summarizer.null_words = null_words
 
-# 設定摘要句數
-summary_sentences = summarizer(parser.document, 3)
+
+summary_sentences = summarizer(parser.document, 1)
+
 summary = " ".join(str(sentence) for sentence in summary_sentences)
 
-print("\n摘要結果：\n", summary)
+print(summary)
